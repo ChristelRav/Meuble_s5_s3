ee689a7945a5e533b2cc85744dc0f09528bb0ff9
ChristelRav <ravmihary@gmail.com>
2023-12-14 13:26:02 +0300

    base_meuble1.sql

--- H_Meuble/build/web/WEB-INF/classes/bdd/MEUBLE.sql
+++ H_Meuble/build/web/WEB-INF/classes/bdd/MEUBLE.sql
@@ -1,35 +1,72 @@
-create database meuble;
+CREATE DATABASE meuble;
 \c meuble
-
 
 CREATE TABLE categorie (
     idCategorie SERIAL PRIMARY KEY,
     categorie VARCHAR(155)
 );
+
+CREATE TABLE modele (
+    idModele SERIAL PRIMARY KEY,
+    idCategorie INT REFERENCES categorie(idCategorie),
+    modelecol1 VARCHAR(155) DEFAULT 'longueur',
+    modelecol2 VARCHAR(155) DEFAULT 'largeur',
+    modelecol3 VARCHAR(155), -- HAUTEUR// NB PLACE (Canapé)
+    modelecol4 VARCHAR(155) DEFAULT 'temps Fabrication'
+);
+
+CREATE TABLE sousCategorie (
+    idSousCategorie SERIAL PRIMARY KEY,
+    idCategorie INT REFERENCES categorie(idCategorie),
+    longueur DOUBLE PRECISION DEFAULT 15,
+    largeur DOUBLE PRECISION DEFAULT 15,
+    plus DOUBLE PRECISION DEFAULT 15, 
+    tempsFabrication TIME
+);
+
 CREATE TABLE style (
     idStyle SERIAL PRIMARY KEY,
-    style VARCHAR(155)
+    style VARCHAR(155) DEFAULT 'defaut',
+    tempsFabrication TIME DEFAULT CURRENT_TIME
 );
 
-CREATE table meuble (
+CREATE TABLE meuble (
     idMeuble SERIAL PRIMARY KEY,
-    idCategorie INT REFERENCES categorie(idCategorie),
-    idStyle INT REFERENCES style(idStyle),
-    meuble VARCHAR(155),
-    taille INT,
-    prix DOUBLE PRECISION,
-    tps_fabrication TIME
+    idSousCategorie INT REFERENCES sousCategorie(idSousCategorie),
+    idStyle INT REFERENCES style(idStyle)
 );
 
-CREATE TABLE matiere (
-    idMatiere SERIAL PRIMARY KEY,
-    matiere VARCHAR(255)
+CREATE TABLE materiel (
+    idMateriel SERIAL PRIMARY KEY,
+    materiel VARCHAR(155),
+    unite VARCHAR(155)
 );
 
-CREATE TABLE matiereMeuble (
-    idMatiereMeuble SERIAL PRIMARY KEY,
-    idMatiere INT REFERENCES matiere(idMatiere),
+CREATE TABLE styleMateriel (
+    idStyleMateriel SERIAL PRIMARY KEY,
     idStyle INT REFERENCES style(idStyle),
-    quantite DOUBLE PRECISION,
-    dateMiseJour DATE
+    idMateriel INT REFERENCES materiel(idMateriel)
+);
+
+CREATE TABLE confectionMeuble (
+    idConfectionMeuble SERIAL PRIMARY KEY,
+    idMeuble INT REFERENCES meuble(idMeuble),
+    idMateriel INT REFERENCES materiel(idMateriel),
+    quantiteUtilisee DOUBLE PRECISION DEFAULT 0
+);
+
+CREATE TABLE miseEnVente (
+    idMiseEnVente SERIAL PRIMARY KEY,
+    idMeuble INT REFERENCES meuble(idMeuble),
+    quantiteVente DOUBLE PRECISION DEFAULT 0,
+    dateVente DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE meubleConfectionne (
+    idMeubleConfectionne SERIAL PRIMARY KEY,
+    idMeuble INT REFERENCES meuble(idMeuble),
+    quantite DOUBLE PRECISION DEFAULT 0,
+    quantiteMvt DOUBLE PRECISION DEFAULT 0,
+    prixUnitaire DOUBLE PRECISION DEFAULT 0,
+    dateConfection DATE DEFAULT CURRENT_DATE
 );
